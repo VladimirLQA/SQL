c42d0a660060fad7056aff15732bd8a5d5603ad5
VladimirLQA <mr.kfpehtyrj@mail.ru>
2023-01-07 17:15:13 +0200

    plus solved task

--- tasks_MySQL_stepik_3.2.sql
+++ tasks_MySQL_stepik_3.2.sql
@@ -45,3 +45,18 @@ UPDATE attempt
                          WHERE attempt_id = 8)
         WHERE attempt_id = 8;
 SELECT * FROM attempt;
+
+/*Not my solution, but interesting one*/
+
+SET @last_attempt = (SELECT MAX(attempt_id) FROM attempt);
+SET @last_result = (
+        SELECT ROUND(AVG(is_correct)*100, 0) AS result
+        FROM testing
+        JOIN answer USING(answer_id)
+        WHERE attempt_id = @last_attempt);
+
+UPDATE attempt
+    SET result = @last_result
+        WHERE attempt_id = @last_attempt
+
+---------------------------------------------------------------------------------------------------------------------------------------
